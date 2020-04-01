fe2a449d16b3a8c1603066b0a538d46d4a735fde
Nicole Simpson <nicolesimpson@Nicoles-MacBook-Pro-2.local>
2020-03-31 20:19:39 -0400

    added happy shopping button to indicate bottom of page

--- js/index.js
+++ js/index.js
@@ -10,3 +10,18 @@ function moveScrollIndicator() {
     const percentage = ((window.scrollY) / maxScrollableHeight) * 100;
     scrollIndicatorElt.style.width = percentage + '%';
 }
+
+//Nav minimize on scroll, give users more room to read article and makes sections clear while at top.
+window.onscroll = function() { scrollFunction()};
+function scrollFunction() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").style.fontSize = "20px";
+    } else {
+        document.getElementById("nav").style.fontSize = "40px";
+    }
+}
+
+//Happy shopping message button that indicates to the user that they are dont on that page
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
